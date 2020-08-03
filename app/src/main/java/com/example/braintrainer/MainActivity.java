ea80ad14776e7eb17ed3e0b75ed726ec22fcc638
sushiz <sushiz@users.noreply.github.com>
2020-08-03 20:38:03 +0800

    Random numbers generated

--- app/src/main/java/com/example/braintrainer/MainActivity.java
+++ app/src/main/java/com/example/braintrainer/MainActivity.java
@@ -9,11 +9,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     CountDownTimer timer;
     View linearLayout, gridLayout, imageView, restartButton;
-    TextView finalText;
+    TextView finalText, option1, option2, option3, option4;
+    int sum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +28,15 @@ public class MainActivity extends AppCompatActivity {
         imageView = findViewById(R.id.imageView);
         finalText = findViewById(R.id.finalScoreTextView);
         restartButton = findViewById(R.id.restartButton);
+        option1 = findViewById(R.id.option1TextView);
+        option2 = findViewById(R.id.option2TextView);
+        option3 = findViewById(R.id.option3TextView);
+        option4 = findViewById(R.id.option4TextView);
 
         final TextView timerText = findViewById(R.id.countDownTextView);
         timer = new CountDownTimer(5000, 1000) {
             @Override
             public void onTick(long l) {
-                //update timer view
                 String timeLeft = l / 1000 +"s";
                 timerText.setText(timeLeft);
             }
@@ -51,8 +57,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void checkAnswer(View view){
-        //check answer
-        String answer= view.getTag().toString();
+
+        TextView clickedView = (TextView) view;
+        String answer = clickedView.getTag().toString();
         Log.i("info", answer);
         //update score
         //new question
@@ -60,9 +67,31 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void generateQuestion(){
-        //generate question and answer
+        int num1, num2, shuffle;
+        String n1,n2,s;
+        //generate question
+        num1 = (int)(Math.random()*50); // random number from 0 to 50
+        n1 = String.valueOf(num1);
+        num2 = (int)(Math.random()*50); //random number from 0 to 50
+        n2 = String.valueOf(num2);
+        sum = num1 + num2;
+        s = String.valueOf(sum);
+        TextView question = findViewById(R.id.questionTextView);
+        question.setText(n1 + " + " + n2 +" = ?" );
+        //generate array of 4 wrong answers
+        String[] options = new String[4];
+        for(int i = 0; i<options.length; i++){
+            options[i] = String.valueOf((int)(Math.random()*99));
+
+            Log.i("options",options[i]);
+        }
+        //replace a random value with the right answer
+        Random r = new Random();
+        shuffle = r.nextInt(4) + 1; //random number from 1 to 4
+        options[shuffle] = s;
+
+        Log.i("answer block", String.valueOf(shuffle));
         //generate wrong answers
-        //shuffle answers
         //fill options
     }
 
@@ -75,6 +104,7 @@ public class MainActivity extends AppCompatActivity {
         imageView.setVisibility(View.GONE);
         finalText.setVisibility(View.GONE);
         restartButton.setVisibility(View.GONE);
+        generateQuestion();
         timer.start();
     }
 }
